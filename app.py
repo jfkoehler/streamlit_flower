e98321d445541763d8b3b89d83c69ece503c1e14
jfkoehler <koehlerj@newschool.edu>
2022-12-15 11:42:23 -0500

    after class

--- app.py
+++ app.py
@@ -1,29 +1,78 @@
-import streamlit as st 
-import plotly.express as px 
-import pandas as pd 
+import streamlit as st
+import plotly.express as px
 import numpy as np
 import pickle
+from predict import predict_flower
+import time
 
-st.title("This is my flower predictor.")
-st.header("This is a great app")
-st.subheader("Flowers are my fav.")
 
-df = px.data.iris() 
+st.title("My Iris Predictor App")
+st.header("It predicts Iris flower types")
+st.subheader("Cool, huh?")
+
+df_iris = px.data.iris()
+
+# must save as variable 
+hist_sepal = px.histogram(df_iris, x='sepal_length')
+hist_sepal    
+
+
+# 
+df = px.data.gapminder()
+
+fig = px.choropleth(
+    df,
+    locations="iso_alpha",
+    color="lifeExp",
+    hover_name="country",
+    animation_frame="year",  # animation
+    range_color=[20, 80],
+)
+fig
 
 show_df = st.checkbox("Do you want to see the data?")
+# show_df  # boolean
 
-if show_df:
-    st.write(df)
+# st.write(type(show_df))
 
-s_l = st.number_input("Sepal Length (cm)", 0, 100)
-s_w = st.number_input("Sepal Width (cm)", 0, 100)
-p_l = st.number_input("Petal Length (cm)", 0, 100)
-p_w = st.number_input("Petal Width (cm)", 0, 100)
+if show_df:  # evaluates to true if checked
+    df_iris
 
-user_input = np.array([s_l, s_w, p_l, p_w]).reshape(1, -1)
+s_l = st.number_input("Sepal Length in cm", 0, 100)
+s_w = st.number_input("Sepal Width in cm", 0, 100)
+p_l = st.number_input("Petal Length in cm", 0, 100)
+p_w = st.number_input("Petal Width in cm", 0, 100)
 
-with open('saved-iris-model.pkl', 'rb') as flower_pickle:
-    model = pickle.load(flower_pickle)
+user_input = np.array([s_l, s_w, p_l, p_w])
+user_input
 
-prediction = model.predict(user_input)
-st.write(f'The predicted flower is {prediction[0]}')
+# load model
+with open('saved-iris-model.pkl', 'rb') as f:
+    classifier = pickle.load(f)
+
+# make prediction
+with st.spinner('Predicting...'):
+    time.sleep(5)
+    prediction = predict_flower(classifier, user_input)
+
+st.header(f"The model predicts: {prediction[0]}")
+
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    if prediction[0]=='versicolor':
+        st.header("An owl")
+        st.image("https://static.streamlit.io/examples/owl.jpg")
+    else:
+        st.image("https://static.streamlit.io/examples/dog.jpg")
+
+st.balloons()
